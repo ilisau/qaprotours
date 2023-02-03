8ee0df796c099a2655fc081d74f0d60d3847524f
ilisau <ilisov.laba@solvd.com>
2023-02-03 18:07:33 +0300

    feat: Initialize project, add dependencies, Spring and Docker configs

---
+++ src/main/java/com/solvd/qaprotours/QaprotoursApplication.java
@@ -0,0 +1,13 @@
+package com.solvd.qaprotours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaprotoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaprotoursApplication.class, args);
+	}
+
+}
