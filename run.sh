ca6748e2532b8b9f429d35036a9dfffaea32ff3d
ilisau <ilisov.laba@solvd.com>
2023-02-23 13:09:47 +0300

    closes #27

---
+++ run.sh
@@ -0,0 +1,15 @@
+cd src/infra || exit
+kubectl apply -f minio-secrets.yml
+kubectl apply -f pg-secrets.yml
+kubectl apply -f qaprotours-secrets.yml
+kubectl apply -f qaprotours-configmap.yml
+
+kubectl apply -f qaprotours-service.yml
+kubectl apply -f minio-service.yml
+kubectl apply -f pg-service.yml
+
+kubectl apply -f qaprotours-deployment.yml
+kubectl apply -f pg-stateful.yml
+kubectl apply -f minio-stateful.yml
+
+kubectl minikube service qaprotours
