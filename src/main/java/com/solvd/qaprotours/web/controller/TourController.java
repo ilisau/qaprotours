68d4f66c78b43739223946b85b7da16847d7a739
ilisau <ilisov.laba@solvd.com>
2023-02-28 13:44:46 +0300

    Implemented Resilience4j Circuit Breaker

--- src/main/java/com/solvd/qaprotours/web/controller/TourController.java
+++ src/main/java/com/solvd/qaprotours/web/controller/TourController.java
@@ -1,6 +1,9 @@
 package com.solvd.qaprotours.web.controller;
 
 import com.solvd.qaprotours.domain.Image;
+import com.solvd.qaprotours.domain.exception.AuthException;
+import com.solvd.qaprotours.domain.exception.ImageUploadException;
+import com.solvd.qaprotours.domain.exception.ServiceNotAvailableException;
 import com.solvd.qaprotours.domain.tour.Tour;
 import com.solvd.qaprotours.domain.tour.TourCriteria;
 import com.solvd.qaprotours.service.ImageService;
@@ -13,8 +16,10 @@ import com.solvd.qaprotours.web.dto.validation.ValidateExtension;
 import com.solvd.qaprotours.web.mapper.ImageMapper;
 import com.solvd.qaprotours.web.mapper.TourCriteriaMapper;
 import com.solvd.qaprotours.web.mapper.TourMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -34,6 +39,7 @@ public class TourController {
     private final TourMapper tourMapper;
     private final TourCriteriaMapper tourCriteriaMapper;
     private final ImageMapper imageMapper;
+    private final String IMAGE_SERVICE = "imageService";
 
     @GetMapping
     public List<TourDto> getAll(@RequestParam(required = false) Integer currentPage,
@@ -78,10 +84,21 @@ public class TourController {
     @PostMapping("/{tourId}/photo")
     @PreAuthorize("hasRole('EMPLOYEE')")
     @ValidateExtension
+    @CircuitBreaker(name = IMAGE_SERVICE, fallbackMethod = "handleError")
     public void uploadImage(@PathVariable Long tourId,
                             @ModelAttribute ImageDto dto) {
         Image image = imageMapper.toEntity(dto);
         imageService.uploadImage(tourId, image);
+    }
+
+    private void handleError(Exception e) {
+        if (e.getClass().equals(AccessDeniedException.class)) {
+            throw new AuthException(e.getMessage());
+        }
+        if (e.getClass().equals(ImageUploadException.class)) {
+            throw new ImageUploadException(e.getMessage());
+        }
+        throw new ServiceNotAvailableException();
     }
 
 }
