bdde2c80bfe7e2e5d285e5878a3a8ceb4ad7f616
Ilya Lisov <ilisov@solvd.com>
2023-04-26 14:37:24 +0300

    #58 Implement Strimzi

--- kafka-setup.sh
+++ kafka-setup.sh
@@ -1,6 +1,6 @@
 kubectl create namespace kafka
-kubectl apply -f https://strimzi.io/install/latest?namespace=kafka -n kafka
-kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml -n kafka
+kubectl apply -f https://strimzi.io/install/0.34.0?namespace=kafka -n kafka
+kubectl apply -f https://strimzi.io/examples/0.34.0/kafka/kafka-persistent-single.yaml -n kafka
 
 kubectl create sa kafka-serviceaccount
 kubectl apply -f src/infra/kafka-clusterrolebinding.yml
