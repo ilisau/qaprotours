fcc021ef69e8651bc5e0707282af1eddd1762c43
Ilya Lisov <ilisov@solvd.com>
2023-05-15 14:16:56 +0300

    #60 Refactor after review

--- src/main/java/com/solvd/qaprotours/config/kafka/MessageReceiverImpl.java
+++ src/main/java/com/solvd/qaprotours/config/kafka/MessageReceiverImpl.java
@@ -1,17 +1,19 @@
 package com.solvd.qaprotours.config.kafka;
 
-import com.solvd.qaprotours.config.kafka.elasticsearch.HandlersMap;
+import com.solvd.qaprotours.config.kafka.elasticsearch.Handler;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.kafka.receiver.KafkaReceiver;
+
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class MessageReceiverImpl implements MessageReceiver {
 
     private final KafkaReceiver<String, Object> receiver;
-    private final HandlersMap handlersMap;
+    private final Map<String, Handler> handlersMap;
 
     /**
      * Initialize receiver.
@@ -24,7 +26,11 @@ public class MessageReceiverImpl implements MessageReceiver {
     @Override
     public void fetch() {
         receiver.receive()
-                .subscribe(handlersMap::handleMessage);
+                .subscribe(r -> {
+                    for (Handler handler : handlersMap.values()) {
+                        handler.handleMessage(r);
+                    }
+                });
     }
 
 }
