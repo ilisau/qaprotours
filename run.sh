837f513bf879d1d03c7854ab336388a08f6c10ef
Ilya Lisov <ilisov@solvd.com>
2023-05-23 13:41:58 +0300

    #70 Add K8s configs

--- run.sh
+++ run.sh
@@ -4,15 +4,20 @@ kubectl apply -f qaprotours-service-account.yml
 kubectl apply -f minio-secrets.yml
 kubectl apply -f pg-secrets.yml
 kubectl apply -f qaprotours-configmap.yml
+kubectl apply -f grafana-secrets.yml
 
 kubectl apply -f qaprotours-service.yml
 kubectl apply -f minio-service.yml
 kubectl apply -f pg-service.yml
 kubectl apply -f elastic-service.yml
 kubectl apply -f kibana-service.yml
+kubectl apply -f grafana-service.yml
+kubectl apply -f prometheus-service.yml
 
 kubectl apply -f qaprotours-deployment.yml
 kubectl apply -f pg-stateful.yml
 kubectl apply -f minio-stateful.yml
 kubectl apply -f elastic-stateful.yml
 kubectl apply -f kibana-deployment.yml
+kubectl apply -f grafana-stateful.yml
+kubectl apply -f prometheus-stateful.yml
